fa4a0a06f971545a219bf9c72cbecc81ef06f7b2
ayden <aydendiel@gmail.com>
2025-07-17 19:46:02 -0500

    feat: added history

--- src/index.js
+++ src/index.js
@@ -1,31 +1,52 @@
-const LANDING_PAGE = 'aydendiel.dev';
+const START_URL = 'lobste.rs';
+
+const cache = {};
 
 const urlInput = document.querySelector('#url-input');
 const inputForm = document.querySelector('#input-form');
 const container = document.querySelector('#content');
 
-async function loadPage(url) {
+async function parseUrl(url) {
+  if (cache.hasOwnProperty(url)) {
+    return cache[url];
+  }
   const markdown = await fetch('https://r.jina.ai/' + url)
     .then(response => response.text());
   const html = marked.parse(markdown)
     .replaceAll(
       /<a href="(https?:\/\/[^"]+)">([\s\S]*?)<\/a>/g,
       (_, url, text) =>
-        `<a href="#" onclick="handleLinkClick(event, '${url}');">${text}</a><a href="${url}">+</a>`
+        `<a href="#" onclick="handleLinkClick(event, '${url}');">${text}</a>(<a href="${url}" target="_blank">+</a>)`
     )
+  cache[url] = html;
+  return html;
+}
+
+async function loadPage(url) {
+  content.innerHTML = `loading <em><a href="${url}">${url}</a></em>`
+  console.log(`loading ${url}`)
+  const html = await parseUrl(url);
   content.innerHTML = html;
+  console.log(`loaded.`);
 }
 
 async function handleLinkClick(e, url) {
-  e.preventDefault();
+  e?.preventDefault();
   await loadPage(url);
+  history.pushState(url, url);
 }
 
 window.onload = () => {
-  loadPage(LANDING_PAGE);
+  handleLinkClick(null, START_URL);
 }
 
-inputForm.onsubmit = (e) => {
+window.addEventListener('popstate', (e) => {
   e.preventDefault();
-  loadPage(urlInput.value);
+  loadPage(e.state);
+})
+
+inputForm.onsubmit = async (e) => {
+  e.preventDefault();
+  await loadPage(urlInput.value);
+  history.pushState(url, url)
 }
